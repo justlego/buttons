563855c12e53838fd1c8b73baadd829816cbe187
Apple <apple@cdy.local>
2016-05-25 11:06:37 +0800

    first commit

--- buttons/ViewController.m
+++ buttons/ViewController.m
@@ -6,17 +6,63 @@
 //  Copyright © 2016年 Apple. All rights reserved.
 //
 
+#define ScreenWidth self.view.frame.size.width
+#define ScreenHeight self.view.frame.size.height
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray *buttonTitleArr;
+@property (nonatomic, strong) UIView *lineView;
 
 @end
 
 @implementation ViewController
 
+- (NSArray *)buttonTitleArr
+{
+    if (!_buttonTitleArr)
+    {
+        _buttonTitleArr = [NSArray arrayWithObjects:@"1",@"2",@"3", nil];
+    }
+    return _buttonTitleArr;
+}
+
+- (UIButton *)createButtonWithTitle:(NSString *)title frame:(CGRect)frame backColor:(UIColor *)color
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:title forState:UIControlStateNormal];
+    button.frame = frame;
+    [button setBackgroundColor:color];
+    return button;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self buttonTitleArr];
+    
+    
+    
+    for (int count = 0; count < _buttonTitleArr.count; count ++)
+    {
+        CGRect frame = CGRectMake(ScreenWidth * count/_buttonTitleArr.count, 64, ScreenWidth/_buttonTitleArr.count, 40);
+        UIButton *button = [self createButtonWithTitle:_buttonTitleArr[count] frame:frame backColor:[UIColor redColor]];
+        button.tag = count + 1000;
+        [button addTarget:self action:@selector(clicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:button];
+    }
+}
+
+- (void)clicked:(UIButton *)button
+{
+    [_lineView removeFromSuperview];
+    _lineView = [[UIView alloc] init];
+    _lineView.frame = CGRectMake(button.frame.origin.x, button.frame.origin.y + button.frame.size.height - 2, button.frame.size.width, 2);
+    _lineView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:_lineView];
+    NSLog(@"%lu",button.tag);
 }
 
 - (void)didReceiveMemoryWarning {
